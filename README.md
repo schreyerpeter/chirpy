ecf6a8fc30378b54ed63a06ebba94837e81b1414
schreyerpeter <prhschreyer@gmail.com>
2025-12-03 09:55:19 -0800

    feat: small housekeeping changes

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-### To run DB migration, from sql/schema run
+## About Chirpy
+
+- Chirpy is a simple clone of Twitter's basic functionality, built with Go
+- Basic auth and resource management endpoints are included
+
+## Development Prompts
+
+### To run DB migration, from sql/schema:
 
 - `goose postgres "postgres://postgres:postgres@localhost:5432/chirpy" up`
 
-### To generate new models after creating/updating queries run
+### To generate new models after creating/updating queries:
 
 - `sqlc generate`
 
--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +14,28 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	}
+
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
+
 		chirps = append(chirps, Chirp{
 			ID:        dbChirp.ID,
 			CreatedAt: dbChirp.CreatedAt,
@@ -21,6 +44,13 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 			Body:      dbChirp.Body,
 		})
 	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		}
+		return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+	})
 
 	respondWithJSON(w, http.StatusOK, chirps)
 }
